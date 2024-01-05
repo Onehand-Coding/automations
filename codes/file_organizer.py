2c56045b85d86a29ad0f20d7cf48f2ed13f4f019
Kenneth <kennethalisen433@protonmail.com>
2024-01-05 12:23:19 +0800

    file_organizer.py - Added stem_sort, split_stem, clean_stem, get_all_files, get_all_foldersand get_common_prefix function.

--- codes/file_organizer.py
+++ codes/file_organizer.py
@@ -1,10 +1,15 @@
 import re
 import shutil
-import stat
 from enum import Enum
+from itertools import combinations
 from collections import defaultdict
 
+from send2trash import send2trash
+
 from helper import get_folder_path, new_filepath, confirm
+
+TO_REPLACE_PATTERN = re.compile(r'\[.*\]|\(.*\)')
+SPLIT_PATTERN = re.compile(r'\s+|[.,:_-]')
 
 
 class FileType(Enum):
@@ -36,22 +41,119 @@ def map_files(files, to_sort_path):
 
 
 def move_file(file, dest):
-    if not dest.exists():
-        dest.mkdir(parents=True, exist_ok=True)
+    if file.exists():
+        if not dest.exists():
+            dest.mkdir(parents=True, exist_ok=True)
 
-    destination_file = dest / file.name
-    if destination_file.exists() and not file.samefile(destination_file):
-        destination_file = new_filepath(destination_file, add_prefix='_Duplicate')
+        destination_file = dest / file.name
+        if destination_file.exists() and not file.samefile(destination_file):
+            destination_file = new_filepath(destination_file, add_prefix='_Duplicate')
+        try:
+            shutil.move(file, destination_file)
+        except FileNotFoundError:
+            pass
 
-    shutil.move(file, destination_file)
+
+def verify_files(unsorted_files, sorted_files, method='Agressive'):
+    is_ok = len(sorted_files) == len(unsorted_files)
+    if is_ok:
+        print(f"{method} sort successful!")
+    else:
+        removed_files = set(unsorted_files) - sorted_files
+        if removed_files:
+            print(f"Some files are Removed!")
+            for file in removed_files:
+                print(file.name)
+
+
+def remove_folders(folders):
+    for folder in folders:
+        send2trash(str(folder))
+
+
+def should_exclude(file, to_exclude_files):
+    return any(to_exclude in file.parts for to_exclude in to_exclude_files)
+
+
+def split_stem(split_pattern, stem):
+    return re.split(split_pattern, stem)
+
+
+def clean_stem(to_replace_pattern, stem, replacement=''):
+    return re.sub(to_replace_pattern, replacement, stem).strip()
+
+
+def get_all_folders(to_sort_path, to_exclude_files):
+    return {
+        folder for folder in to_sort_path.rglob("*/")
+        if not should_exclude(folder, to_exclude_files)
+    }
+
+
+def get_all_files(to_sort_path, to_exclude_files):
+    return {
+        file for file in to_sort_path.rglob("*")
+        if file.is_file() and not should_exclude(file, to_exclude_files)
+    }
+
+
+def get_common_prefixes(files):
+    split_file_stems = [split_stem(SPLIT_PATTERN, clean_stem(TO_REPLACE_PATTERN, file.stem)) for file in files]
+
+    common_prefixes = []
+    for x_combination, y_combination in combinations(split_file_stems, 2):
+        common_prefix = []
+        for i in range(len(x_combination)):
+            first_word = x_combination[0]
+            try:
+                if x_combination[i] == y_combination[i]:
+                    common_prefix.append(x_combination[i])
+            except IndexError:
+                pass
+        try:
+            common_prefix = common_prefix[:common_prefix.index(first_word, 1)]
+        except (ValueError, IndexError):
+            pass
+        common_prefixes.append(tuple(common_prefix))
+
+    common_prefixes = list(set(common_prefixes))
+    common_prefixes = [
+        list(common_prefix)
+        for common_prefix in common_prefixes
+        if common_prefix and len(set(prefix.lower() for prefix in common_prefix)) > 1
+    ]
+    return common_prefixes
+
+
+def stem_sort(to_sort_path):
+    unsorted_files = [file for file in to_sort_path.glob("*") if file.is_file()]
+    common_prefixes = get_common_prefixes(unsorted_files)
+
+    prefix_folders = defaultdict(list)
+    for file in unsorted_files:
+        split_file_stem = split_stem(SPLIT_PATTERN, clean_stem(TO_REPLACE_PATTERN, file.stem))
+        for common_prefix in common_prefixes:
+            try:
+                if (all(common_prefix[i] == split_file_stem[i] for i in range(len(common_prefix)))):
+                    prefix_folders[' '.join(common_prefix)].append(file)
+            except IndexError:
+                pass
+
+    if len(prefix_folders.keys()) > 1:
+        for prefix, files in prefix_folders.items():
+            if len(files) > 1:
+                for file in files:
+                    if prefix not in file.parts:
+                        prefix_folder = file.parent / prefix
+                        move_file(file, prefix_folder)
 
 
 def prefix_sort(to_sort_path):
-    unsorted_files = {file for file in to_sort_path.glob("*") if file.is_file()}
+    unsorted_files = [file for file in to_sort_path.glob("*") if file.is_file()]
     prefix_folders = defaultdict(list)
 
     for file in unsorted_files:
-        prefix = re.split(r"\s+|[._-]", file.stem)[0]
+        prefix = split_stem(SPLIT_PATTERN, clean_stem(TO_REPLACE_PATTERN, file.stem))[0]
         prefix_folders[prefix].append(file)
 
     if len(prefix_folders.keys()) > 1:
@@ -78,41 +180,10 @@ def simple_sort(to_sort_path):
     print("Simple sort Done!")
 
 
-def verify_removed(unsorted_files, sorted_files):
-    removed_files = set(unsorted_files) - sorted_files
-    if removed_files:
-        print(f"Some files are Removed!")
-        for file in removed_files:
-            print(file.name)
-
-
-def remove_folders(folders):
-    def remove_readonly(func, path, _):
-        """Clear the readonly bit and reattempt the removal"""
-        path.chmod(path.stat().st_mode | stat.S_IWRITE)
-        func(path)
-
-    for folder in folders:
-        try:
-            shutil.rmtree(folder)
-        except FileNotFoundError:
-            pass
-        except PermissionError:
-            if confirm(f"Forcing deletion of this folder: {folder}", confirm_letter="yes"):
-                shutil.rmtree(folder, onerror=remove_readonly)
-
-
-def should_exclude(file, to_exclude_files):
-    return any(to_exclude in file.parts for to_exclude in to_exclude_files)
-
-
 def agressive_sort(to_sort_path, to_exclude_files=None):
     if to_exclude_files is None:
         to_exclude_files = []
-    unsorted_files = {
-        file for file in to_sort_path.rglob("*")
-        if file.is_file() and not should_exclude(file, to_exclude_files)
-    }
+    unsorted_files = get_all_files(to_sort_path, to_exclude_files)
     mapped_files = map_files(unsorted_files, to_sort_path)
     default_folders = set()
 
@@ -122,30 +193,29 @@ def agressive_sort(to_sort_path, to_exclude_files=None):
         move_file(file, ext_dest)
         default_folders.update({type_dest, ext_dest})
 
-    all_folders = {
-        folder for folder in to_sort_path.rglob("*/")
-        if not should_exclude(folder, to_exclude_files)
-    }
+    all_folders = get_all_folders(to_sort_path, to_exclude_files)
     empty_folders = all_folders - default_folders
     remove_folders(empty_folders)
 
     for folder in all_folders:
         prefix_sort(folder)
 
-    sorted_files = {
-        file for file in to_sort_path.rglob("*")
-        if file.is_file() and not should_exclude(file, to_exclude_files)
-    }
-    is_ok = len(sorted_files) == len(unsorted_files)
-    print("Agressive sort successful!" if is_ok else verify_removed(unsorted_files, sorted_files))
+    for i in range(2):
+        for folder in get_all_folders(to_sort_path, to_exclude_files):
+            stem_sort(folder)
+
+    sorted_files = get_all_files(to_sort_path, to_exclude_files)
+    verify_files(unsorted_files, sorted_files)
 
 
 def main():
     to_sort_dir = get_folder_path(task="folder you want to organize files from")
 
     if confirm("Simple sort?", confirm_letter="yes"):
+        print(f'Organizing files in {to_sort_dir.name} ...')
         simple_sort(to_sort_dir)
     elif confirm("Aggressive sort?", confirm_letter="yes"):
+        print(f'Organizing files in {to_sort_dir.name} ...')
         agressive_sort(to_sort_dir)
 
 
